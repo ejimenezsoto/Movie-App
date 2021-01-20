3d31834edd905addaf4adf43884530f723f20411
Enzo Jimenez-Soto <enzojimenezsoto@gmail.com>
2021-01-19 23:13:15 -0800

    Lets see

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import { Header } from "./components/Header";
 import { Watchlist } from "./components/Watchlist";
 import { Watched } from "./components/Watched";
