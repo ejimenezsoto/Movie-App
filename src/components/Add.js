70a55c002ed2876817ada72cbbc7c1065e5e9e7a
Enzo Jimenez-Soto <enzojimenezsoto@gmail.com>
2021-01-20 13:12:03 -0800

    Try again

--- src/components/Add.js
+++ src/components/Add.js
@@ -7,11 +7,10 @@ export const Add = () => {
 
     const onChange = (e) => {
     e.preventDefault();
-
     setQuery(e.target.value);
 
     fetch(
-        `https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&page=1&include_adult=false&query=${e.target.value}`
+        `https://api.themoviedb.org/3/search/movie?api_key=ed58bee5f0cabceea91f76e5ebf5cae8&language=en-US&page=1&include_adult=false&query=${e.target.value}`
     )
         .then((res) => res.json())
         .then((data) => {
